19c8b92b26d15362aa6683044832eb5a98f71736
Jeff Trojan <j.dauda@alustudent.com>
2023-01-25 14:08:30 +0200

    Fix : Fixed responsiveness and added motion to button

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -24,6 +24,67 @@ padding: 3rem calc((100vw - 1300px) /2);
     
     grid-grid-template-columns: 1fr;
 }
+/* For Desktop View */
+@media screen and (min-width: 1024px) {
+  
+    background-color: #131313;
+    color: #fff;
+  
+}
+ 
+/* For Tablet View */
+@media screen and (min-device-width: 768px)
+and (max-device-width: 1024px) {
+  
+  grid-grid-template-columns: 1fr;
+  
+}
+ 
+/* For Mobile Portrait View */
+@media screen and (max-device-width: 480px)
+and (orientation: portrait) {
+  
+  grid-grid-template-columns: 1fr;
+  
+}
+ 
+/* For Mobile Landscape View */
+@media screen and (max-device-width: 640px)
+and (orientation: landscape) {
+  
+  grid-grid-template-columns: 1fr;
+  
+}
+ 
+/* For Mobile Phones Portrait or Landscape View */
+@media screen and (max-device-width: 640px) {
+  
+  grid-grid-template-columns: 1fr;
+  
+}
+ 
+/* For iPhone 4 Portrait or Landscape View */
+@media screen and (min-device-width: 320px)
+and (-webkit-min-device-pixel-ratio: 2) {
+  
+  grid-grid-template-columns: 1fr;
+  
+}
+ 
+/* For iPhone 5 Portrait or Landscape View */
+@media (device-height: 568px) and (device-width: 320px)
+and (-webkit-min-device-pixel-ratio: 2) {
+  
+  grid-grid-template-columns: 1fr;
+  
+}
+ 
+/* For iPhone 6 and 6 plus Portrait or Landscape View */
+@media (min-device-height: 667px) and (min-device-width: 375px)
+and (-webkit-min-device-pixel-ratio: 3) {
+  
+  grid-grid-template-columns: 1fr;
+}
 `;
 
 const ColumnLeft = styled.div`
@@ -47,36 +108,79 @@ p {
 
 
 `;
-const Button = styled.div`
+const Button = styled(motion.button)`
 padding: 1rem 3rem;
 font-size: 1rem;
 border: 2px solid #fff;
-border-radius: 12px;
+border-radius: 10px;
 outline: none;
+cursor: pointer;
 background: transparent;
-
+color: #fff
 `;
 
 const Image = styled.img`
+position: absolute;
+width: 100%;
+height: 100%;
+max-width: 250px;
+max-height: 250px;
 
 `;
 
-const ColumnRight = styled.div``;
+const ColumnRight = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+padding: 2rem;
+position: relative;
+
+${Image}:nth-child(1) {
+  top:10px;
+  left:10px;
+}
+
+${Image}:nth-child(2) {
+  top:170px;
+  right:10px;
+}
+`;
 
 
 const Hero = () => {
+  const Left_fade = {
+    hidden: {opacity: 0, x: -100},
+    visible: {opacity: 1, x: 0}
+  }
   return (
     <Section>
         <Container>
             <ColumnLeft>
-                <h1>Hey, I'm Jeff Dauda!</h1>
-                <p>Welcome to my studio</p>
-                <Button>Let's go!!</Button>
+                <motion.h1  
+                initial={{opacity:0}}
+                animate={{opacity:1}}
+                transition={{duration:1}}
+                >Hey, I'm Jeff Dauda!</motion.h1>
+                <motion.p
+                variants={Left_fade}
+                initial='hidden'
+                animate='visible'
+                transition={{duration:1}}
+                
+                >Welcome to my studio</motion.p>
+                <Button
+                whileHover={{scale:1.15}}
+                whileTap={{scale:0.95}}
+
+
+                >Let's go!!</Button>
             </ColumnLeft>
 
             <ColumnRight>
               
               <Image src={OuterSpace2} alt='planet' />
+              <Image src={OuterSpace1} alt='planet' />
+              
               
             </ColumnRight>
         </Container>
